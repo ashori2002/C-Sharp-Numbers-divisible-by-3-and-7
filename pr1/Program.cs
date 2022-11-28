0a7c602ab70b15a24b5bf6e1ded961e1c4886e68
mohammad ashori <mohammadashori2002@gmail.com>
2022-11-28 23:41:41 +0330

    This simple program written in C# programming language takes two numerical values from the user and prints the numbers divisible by 3 and 7 between these two values.

---
+++ pr1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            Console.WriteLine("Enter number 1 : ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2 : ");
+            ;
+            for (number2 = Convert.ToInt32(Console.ReadLine()); number1 < number2;number2--)
+            {
+
+                if(number2%7==0 && number2 % 3 == 0)
+                {
+                    Console.ForegroundColor = (ConsoleColor)2;
+                    Console.WriteLine(number2 + "true");
+                }
+                else
+                {
+                    Console.ForegroundColor = (ConsoleColor)4;
+                    Console.WriteLine(number2 + "false");
+                }
+            }
+
+
+
+        }
+    }
+}
